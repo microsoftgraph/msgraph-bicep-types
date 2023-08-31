0947e64589daf793b493b42329d91a4713543ca9
Sofia Dalia Magallon Paramo <t-daliam@microsoft.com>
2023-08-31 16:15:19 -0700

    Remove unused definitions
    Complex Types have little difference with entityTypes metadata-wise, so it makes much more sense to use the same code behavior as entityTypes for the output of their definitions

--- swagger-generation/src/definitions/DefinitionMap.ts
+++ swagger-generation/src/definitions/DefinitionMap.ts
@@ -3,14 +3,11 @@
 
 import { EntityType } from "./EntityType";
 import { EnumType } from "./EnumType";
-import { ComplexType } from "./ComplexType";
 import { EntityTypeConfig } from "../config";
 
 export interface EntityMap extends Map<string, EntityType>{}
 
 export interface EnumMap extends Map<string, EnumType>{}
-
-export interface ComplexMap extends Map<string, ComplexType>{}
 
 export interface EntityTypeConfigMap extends Map<string, EntityTypeConfig>{}
 
@@ -19,15 +16,12 @@ export class DefinitionMap{
 
     EntityMap: EntityMap;
     EnumMap: EnumMap;
-    ComplexMap: ComplexMap;
 
     private constructor() {
         const entityMap: EntityMap = new Map<string, EntityType>()
         const enumMap: EnumMap = new Map<string, EnumType>()
-        const complexMap: ComplexMap = new Map<string, ComplexType>()
         this.EntityMap = entityMap;
         this.EnumMap = enumMap;
-        this.ComplexMap = complexMap;
     }  
 
     public static get Instance(): DefinitionMap {
