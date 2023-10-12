fa9761de0e8166834a33ca95f76b75d8336aad5c
Jason Dou <jasondou@microsoft.com>
2023-10-12 13:57:17 -0700

    Add tests for typeloader

--- src/Microsoft.Graph.Bicep.Types.UnitTests/MicrosoftGraphTypeLoaderTests.cs
+++ src/Microsoft.Graph.Bicep.Types.UnitTests/MicrosoftGraphTypeLoaderTests.cs
@@ -1,20 +1,38 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.Graph.Bicep.Types;
 using FluentAssertions;
 using System.Linq;
+using System.Collections.Generic;
+using Azure.Bicep.Types.Concrete;
 
 namespace Microsoft.Graph.Bicep.Types.UnitTests
 {
     [TestClass]
     public class MicrosoftGraphTypeLoaderTests
     {
+        private IReadOnlySet<string> availableTypes = new HashSet<string>
+        {
+            "Microsoft.Graph/applications@beta",
+            "Microsoft.Graph/servicePrincipals@beta",
+            "Microsoft.Graph/groups@beta",
+            "Microsoft.Graph/appRoleAssignedTo@beta",
+            "Microsoft.Graph/oauth2PermissionGrants@beta",
+        };
+
+        private IReadOnlySet<string> typesWithRequiredName = new HashSet<string>
+        {
+            "Microsoft.Graph/applications@beta",
+            "Microsoft.Graph/groups@beta",
+        };
+
         [TestMethod]
         public void MSGraphTypeLoader_can_load_all_types_without_throwing()
         {
             var typeLoader = new MicrosoftGraphTypeLoader();
             var index = typeLoader.LoadTypeIndex();
+
+            index.Resources.Keys.Should().NotBeEmpty();
 
             foreach (var kvp in index.Resources)
             {
@@ -34,7 +52,33 @@ namespace Microsoft.Graph.Bicep.Types.UnitTests
         }
 
         [TestMethod]
-        public void TypeLoader_type_keys_are_insensitively_unique()
+        public void MSGraphTypeLoader_can_load_resources_and_properties()
+        {
+            var typeLoader = new MicrosoftGraphTypeLoader();
+            var index = typeLoader.LoadTypeIndex();
+
+            foreach (var kvp in index.Resources)
+            {
+                availableTypes.Should().Contain(kvp.Key);
+
+                var resourceType = typeLoader.LoadResourceType(kvp.Value);
+                resourceType.Body.Should().NotBeNull();
+
+                var objectType = resourceType.Body.Type as ObjectType;
+                objectType.Should().NotBeNull();
+
+                // check count of properties in body type
+                var properties = objectType!.Properties;
+                properties.Should().NotBeNull();
+                properties.Should().HaveCountGreaterThan(4); // Should at least have id, name, type, and apiVersion
+
+                var nameFlags = typesWithRequiredName.Contains(kvp.Key) ? ObjectTypePropertyFlags.Required | ObjectTypePropertyFlags.DeployTimeConstant : ObjectTypePropertyFlags.None;
+                properties["name"].Flags.Should().HaveFlag(nameFlags);
+            }
+        }
+
+        [TestMethod]
+        public void MSGraphTypeLoader_type_keys_are_insensitively_unique()
         {
             var typeLoader = new MicrosoftGraphTypeLoader();
             var index = typeLoader.LoadTypeIndex();
